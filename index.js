904fa47bef991a8bca3235e550c6a667909cd520
bagoestyo <bagoes.tyo@gmail.com>
2022-01-20 22:36:11 +0700

    Post DB Register

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const http = require("http");
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
-const server = http.createServer(app);
+const http = require("http"); // 1. Untuk create server
+const express = require("express"); // 2. Import
+const app = express(); // 3. Running express, bisa baca json
+const mysql = require("mysql"); // 4. Connect database
+const server = http.createServer(app); // 5.
 const cors = require("cors");
 
 const PORT = 8000;
@@ -10,19 +10,22 @@ const PORT = 8000;
 app.use(express.json());
 app.use(cors());
 
-//Database
+//Database ganti*
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "test",
-  password: "test",
-  database: "db_project",
+  user: "root",
+  password: "tyo06081997",
+  database: "db_user_register",
 });
 
-//Controller
-app.get("/", (req, res) => {
-  connection.query("SELECT * FROM user;", (err, result) => {
-    res.status(200).send(result);
-  });
+//Controller post aja*
+app.post("/", (req, res) => {
+  connection.query(
+    "INSERT INTO `db_user_register`.`data_user` (`username`, `password`, `email`) VALUES ('Arif', 'syaogi', 'syaogi@mail.com');",
+    (err, result) => {
+      res.status(200).send(result);
+    }
+  );
 });
 
 connection.connect();
@@ -30,3 +33,8 @@ connection.connect();
 server.listen(PORT, () => {
   console.log("Socket server is running at port:", PORT);
 });
+
+// POST method route
+// app.post("/", function (req, res) {
+//   res.send("POST request to the homepage");
+// });
